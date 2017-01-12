c463e512e48a4e8dbbd55640909d1e3152815f1f
Amanda Hill <amandabesshill@gmail.com>
2017-01-11 16:29:04 -0800

    Add BaseActivity

---
+++ app/src/main/kotlin/com/thoughtbot/tropos/commons/BaseActivity.kt
@@ -0,0 +1,25 @@
+package com.thoughtbot.tropos.commons
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.thoughtbot.tropos.R
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper
+
+open class BaseActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    CalligraphyConfig.initDefault(CalligraphyConfig.Builder()
+        .setDefaultFontPath("DINNextLTPro-Light.otf")
+        .setFontAttrId(R.attr.fontPath)
+        .build()
+    )
+  }
+
+  override fun attachBaseContext(newBase: Context?) {
+    super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase))
+  }
+}
+
