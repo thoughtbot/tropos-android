a09e1a69dfe1dc4da8c55f80ec4e9fe04e3df848
Amanda Hill <amandabesshill@gmail.com>
2017-01-12 14:43:30 -0800

    Properly parse daily low and high temperature

--- app/src/main/kotlin/com/thoughtbot/tropos/data/remote/WeatherDataService.kt
+++ app/src/main/kotlin/com/thoughtbot/tropos/data/remote/WeatherDataService.kt
@@ -1,16 +1,14 @@
 package com.thoughtbot.tropos.data.remote
 
 import android.location.Location
-import android.util.Log
 import com.thoughtbot.tropos.data.Condition
 import com.thoughtbot.tropos.data.WeatherData
 import com.thoughtbot.tropos.data.WeatherDataSource
-import com.thoughtbot.tropos.data.WindDirection
 import com.thoughtbot.tropos.extensions.WindDirection
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
-import java.util.*
+import java.util.Date
 
 class WeatherDataService(val api: ApiService = RestClient().create(
     ApiService::class.java)) : WeatherDataSource {
@@ -50,8 +48,8 @@ class WeatherDataService(val api: ApiService = RestClient().create(
         val condition = this.currently.icon.mapToCondition()
         val windSpeed = this.currently.windSpeed?.toInt() ?: 0
         val windDirection = WindDirection(this.currently.windBearing ?: 0.0)
-        val lowTemp = this.currently.temperatureMin?.toInt() ?: 0
-        val highTemp = this.currently.temperatureMax?.toInt() ?: 0
+        val lowTemp = this.daily.data[0].temperatureMin?.toInt() ?: 0
+        val highTemp = this.daily.data[0].temperatureMax?.toInt() ?: 0
         val temp = this.currently.temperature?.toInt() ?: 0
 
         WeatherData(date, summary, location, condition, windSpeed, windDirection, lowTemp, temp,
