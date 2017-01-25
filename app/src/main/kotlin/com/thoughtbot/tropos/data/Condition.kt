3a13461396c19a9634f0496022bbf2618b1a5d1e
Amanda Hill <amandabesshill@gmail.com>
2017-01-25 11:37:40 -0800

    Make Condition and Weather Parcelable

--- app/src/main/kotlin/com/thoughtbot/tropos/data/Condition.kt
+++ app/src/main/kotlin/com/thoughtbot/tropos/data/Condition.kt
@@ -1,6 +1,9 @@
 package com.thoughtbot.tropos.data
 
 import android.location.Location
+import android.os.Parcel
+import android.os.Parcelable
+import com.thoughtbot.tropos.extensions.createParcel
 import java.util.*
 
 data class Condition(
@@ -12,4 +15,34 @@ data class Condition(
     val windDirection: WindDirection,
     val lowTemp: Int,
     val currentTemp: Int,
-    val highTemp: Int)
+    val highTemp: Int) : Parcelable {
+
+  companion object {
+    @JvmField val CREATOR = createParcel(::Condition)
+  }
+
+  constructor(source: Parcel) : this(
+      Date(source.readLong()),
+      source.readString(),
+      source.readParcelable<Location>(Location::class.java.classLoader),
+      Status.values()[source.readInt()],
+      source.readInt(),
+      WindDirection.values()[source.readInt()],
+      source.readInt(),
+      source.readInt(),
+      source.readInt())
+
+  override fun describeContents() = 0
+
+  override fun writeToParcel(dest: Parcel?, flags: Int) {
+    dest?.writeLong(date.time)
+    dest?.writeString(summary)
+    dest?.writeParcelable(location, 0)
+    dest?.writeInt(status.ordinal)
+    dest?.writeInt(windSpeed)
+    dest?.writeInt(windDirection.ordinal)
+    dest?.writeInt(lowTemp)
+    dest?.writeInt(currentTemp)
+    dest?.writeInt(highTemp)
+  }
+}
--- app/src/main/kotlin/com/thoughtbot/tropos/data/Weather.kt
+++ app/src/main/kotlin/com/thoughtbot/tropos/data/Weather.kt
@@ -1,5 +1,27 @@
 package com.thoughtbot.tropos.data
 
+import android.os.Parcel
+import android.os.Parcelable
+import com.thoughtbot.tropos.extensions.createParcel
+
 data class Weather(val yesterday: Condition, val today: Condition,
-    val nextThreeDays: List<Condition>)
+    val nextThreeDays: List<Condition>) : Parcelable {
+
+  companion object {
+    @JvmField val CREATOR = createParcel(::Weather)
+  }
+
+  constructor(source: Parcel) : this(
+      source.readParcelable<Condition>(Condition::class.java.classLoader),
+      source.readParcelable<Condition>(Condition::class.java.classLoader),
+      source.createTypedArrayList(Condition.CREATOR))
+
+  override fun describeContents() = 0
+
+  override fun writeToParcel(dest: Parcel?, flags: Int) {
+    dest?.writeParcelable(yesterday, 0)
+    dest?.writeParcelable(today, 0)
+    dest?.writeTypedList(nextThreeDays)
+  }
+}
 
